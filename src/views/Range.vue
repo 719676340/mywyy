e672e6ab0c6f981d5686d60bbe4c744b772cd6b7
719676340 <719676340@qq.com>
2020-07-04 20:08:45 +0800

    调用api获取轮播图mv和歌单的数据

--- src/views/Range.vue
+++ src/views/Range.vue
@@ -1,16 +1,22 @@
 <template>
   <div class=''>
       range
+      <div class="loading-wrapper">
+        <div class="loading"></div>
+        <div class="loading-txt">正在加载中</div>
+      </div>
   </div>
 </template>
 
 <script>
-import {getPersonalized} from '../api/index'
+import {getBannerList, getPersonalized, getPersonalizedMV} from '../api/index'
 export default {
   name: '',
   data () {
     return {
-      personSongList: []
+      bannerList: [],
+      playList: [],
+      mvList: []
     }
   },
   components: {},
@@ -19,11 +25,21 @@ export default {
   },
   mounted () {},
   methods: {
-    async loadDate () {
-      this.personSongList = await getPersonalized()
+    loadDate () {
+      let personSongList = getPersonalized()
+      let BannerList = getBannerList()
+      let PersonalizedMV = getPersonalizedMV()
+      Promise.all([personSongList, BannerList, PersonalizedMV]).then(data => {
+        this.playList = data[0].result.length > 6 ? data[0].result.slice(0, 6) : data[0].result
+        this.bannerList = data[1].banners
+        this.mvList = data[2].result.length > 4 ? data[2].result.slice(0, 6) : data[2].result
+      })
     }
   }
 }
 </script>
 <style>
+/* .loading{
+
+} */
 </style>
